b4aeca0ce1f5ea050adbe4ea4e4dd6059a404a27
Sheila Wittman <szandora789@gmail.com>
2018-05-25 00:07:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,16 +5,16 @@ def turn_count(board)
       turn += 1
     end
   end
-  turn
+  turn #What does this do? Call the turn variable? Show the new turn count?
 end
 
 
 
 
-def current_player(board)
+def current_player(board) #why do I only need the board variable here and not space or turn?
   if turn_count(board) % 2 == 0
-    "X"
+    "X" #Why can't I code this line as puts "X" ?
   else
-    "O"
+    "O" #Why can't I code this as puts "O" ?
   end
 end
